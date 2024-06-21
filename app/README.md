a20330a426c73dc54deee55f398b26995dad486f
Jonas Uekötter <ueman@users.noreply.github.com>
2024-06-21 11:48:04 +0200

    show passes on home page

--- app/README.md
+++ app/README.md
@@ -1,3 +1,4 @@
 # app
 
-A new Flutter project.
+Example app which make use of `passkit` and `passkit_ui` to save and show `PkPass` files.
+Since this is just an example app, this doesn't follow best practices for the sake of simplicity.
--- app/lib/home_page.dart
+++ app/lib/home_page.dart
@@ -1,9 +1,36 @@
+import 'package:app/db/database.dart';
 import 'package:app/import_pass/pick_pass.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:passkit/passkit.dart';
+import 'package:passkit_ui/passkit_ui.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PkPass> passes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadPasses();
+  }
+
+  Future<void> loadPasses() async {
+    final dbPasses = await database.pass.select(distinct: true).get();
+    final mappedPasses = dbPasses.map((p) {
+      return PkPass.fromBytes(p.binaryPass);
+    }).toList();
+    setState(() {
+      passes = mappedPasses;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +59,18 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(child: Text('Cards')),
+      body: passes.isEmpty
+          ? const Center(child: Text('No passes'))
+          : ListView.builder(
+              itemCount: passes.length,
+              itemBuilder: (context, index) {
+                final pass = passes[index];
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: PkPassWidget(pass: pass, onPressed: () {}),
+                );
+              },
+            ),
     );
   }
 }
