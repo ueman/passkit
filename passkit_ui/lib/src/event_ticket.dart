ebeff13b8484626905b852b379900fc173e46ce8
Jonas Uekötter <ueman@users.noreply.github.com>
2024-06-23 10:49:26 +0200

    add background to event ticket

--- passkit_ui/lib/src/event_ticket.dart
+++ passkit_ui/lib/src/event_ticket.dart
@@ -1,8 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:passkit/passkit.dart';
+import 'package:passkit_ui/src/extension/formatting_extensions.dart';
 import 'package:passkit_ui/src/extension/pk_pass_image_extensions.dart';
 import 'package:passkit_ui/src/pass_theme.dart';
-import 'package:passkit_ui/src/extension/formatting_extensions.dart';
 import 'package:passkit_ui/src/widgets/passkit_barcode.dart';
 
 /// Event tickets
@@ -39,99 +41,119 @@ class EventTicket extends StatelessWidget {
 
     return Card(
       color: passTheme.backgroundColor,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                /// The logo image (logo.png) is displayed in the top left corner
-                /// of the pass, next to the logo text. The allotted space is
-                /// 160 x 50 points; in most cases it should be narrower.
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 160,
-                    maxHeight: 50,
-                  ),
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+      child: Stack(
+        children: [
+          if (pass.background != null)
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: ClipRRect(
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                   child: Image.memory(
-                    pass.logo!.forCorrectPixelRatio(context),
-                    fit: BoxFit.contain,
+                    fit: BoxFit.cover,
+                    pass.background!.forCorrectPixelRatio(context),
                   ),
                 ),
-                if (pass.pass.logoText != null)
-                  Text(
-                    pass.pass.logoText!,
-                    style: passTheme.foregroundTextStyle,
-                  ),
-                const Spacer(),
-                // TODO(ueman): The header should be as wide as the thumbnail
-                Column(
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
                   children: [
-                    Text(
-                      eventTicket.headerFields?.first.label ?? '',
-                      style: passTheme.labelTextStyle,
+                    /// The logo image (logo.png) is displayed in the top left corner
+                    /// of the pass, next to the logo text. The allotted space is
+                    /// 160 x 50 points; in most cases it should be narrower.
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        maxWidth: 160,
+                        maxHeight: 50,
+                      ),
+                      child: Image.memory(
+                        pass.logo!.forCorrectPixelRatio(context),
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                    Text(
-                      eventTicket.headerFields?.first.value?.toString() ?? '',
-                      style: passTheme.foregroundTextStyle,
+                    if (pass.pass.logoText != null)
+                      Text(
+                        pass.pass.logoText!,
+                        style: passTheme.foregroundTextStyle,
+                      ),
+                    const Spacer(),
+                    // TODO(ueman): The header should be as wide as the thumbnail
+                    Column(
+                      children: [
+                        Text(
+                          eventTicket.headerFields?.first.label ?? '',
+                          style: passTheme.labelTextStyle,
+                        ),
+                        Text(
+                          eventTicket.headerFields?.first.value?.toString() ??
+                              '',
+                          style: passTheme.foregroundTextStyle,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _AuxiliaryRow(
-                  passTheme: passTheme,
-                  auxiliaryRow: eventTicket.primaryFields!,
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _AuxiliaryRow(
+                      passTheme: passTheme,
+                      auxiliaryRow: eventTicket.primaryFields!,
+                    ),
+                    // The thumbnail image (`thumbnail.png`) displayed next to the
+                    // fields on the front of the pass. The allotted space is
+                    // 90 x 90 points. The aspect ratio should be in the range of
+                    // 2:3 to 3:2, otherwise the image is cropped.
+                    if (pass.thumbnail != null)
+                      Image.memory(
+                        pass.thumbnail!.forCorrectPixelRatio(context),
+                      ),
+                  ],
                 ),
-                // The thumbnail image (`thumbnail.png`) displayed next to the
-                // fields on the front of the pass. The allotted space is
-                // 90 x 90 points. The aspect ratio should be in the range of
-                // 2:3 to 3:2, otherwise the image is cropped.
-                if (pass.thumbnail != null)
+                if (eventTicket.secondaryFields != null)
+                  _AuxiliaryRow(
+                    passTheme: passTheme,
+                    auxiliaryRow: eventTicket.secondaryFields!,
+                  ),
+                if (eventTicket.auxiliaryFields != null) ...[
+                  const SizedBox(height: 16),
+                  _AuxiliaryRow(
+                    passTheme: passTheme,
+                    auxiliaryRow: eventTicket.auxiliaryFields!,
+                  ),
+                ],
+                const SizedBox(height: 16),
+                if (pass.footer != null)
                   Image.memory(
-                    pass.thumbnail!.forCorrectPixelRatio(context),
+                    pass.footer!.forCorrectPixelRatio(context),
+                    fit: BoxFit.contain,
+                    width: 286,
+                    height: 15,
+                  ),
+                if ((pass.pass.barcodes?.firstOrNull ?? pass.pass.barcode) !=
+                    null)
+                  PasskitBarcode(
+                    barcode:
+                        (pass.pass.barcodes?.firstOrNull ?? pass.pass.barcode)!,
+                    passTheme: passTheme,
                   ),
               ],
             ),
-            if (eventTicket.secondaryFields != null)
-              _AuxiliaryRow(
-                passTheme: passTheme,
-                auxiliaryRow: eventTicket.secondaryFields!,
-              ),
-            if (eventTicket.auxiliaryFields != null) ...[
-              const SizedBox(height: 16),
-              _AuxiliaryRow(
-                passTheme: passTheme,
-                auxiliaryRow: eventTicket.auxiliaryFields!,
-              ),
-            ],
-            const SizedBox(height: 16),
-            if (pass.footer != null)
-              Image.memory(
-                pass.footer!.forCorrectPixelRatio(context),
-                fit: BoxFit.contain,
-                width: 286,
-                height: 15,
-              ),
-            if ((pass.pass.barcodes?.firstOrNull ?? pass.pass.barcode) != null)
-              PasskitBarcode(
-                barcode:
-                    (pass.pass.barcodes?.firstOrNull ?? pass.pass.barcode)!,
-                passTheme: passTheme,
-              ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- passkit_ui/lib/src/extension/pk_pass_image_extensions.dart
+++ passkit_ui/lib/src/extension/pk_pass_image_extensions.dart
@@ -6,6 +6,7 @@ import 'package:passkit/passkit.dart';
 extension PkPassImageExtension on PkPassImage {
   Uint8List forCorrectPixelRatio(BuildContext context) {
     final pixelRatio = MediaQuery.devicePixelRatioOf(context).toInt();
-    return fromMultiplier(pixelRatio);
+    // It just looks better when using images a step higher
+    return fromMultiplier(pixelRatio + 1);
   }
 }
