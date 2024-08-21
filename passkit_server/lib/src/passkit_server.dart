010ff4e4a4c1bddb9f7958ca4243fa18c8b70450
Jonas Uekötter <ueman@users.noreply.github.com>
2024-08-21 10:42:15 +0200

    wip

--- passkit_server/lib/src/passkit_server.dart
+++ passkit_server/lib/src/passkit_server.dart
@@ -8,6 +8,16 @@ import 'package:shelf_router/shelf_router.dart';
 extension PasskitServerExtension on Router {
   void addPassKitServer(PassKitBackend backend) {
     get('/v1/passes/<identifier>/<serial>', getLatestVersion);
+    get('/v1/devices/<deviceID>/registrations/<typeID>',
+        getListOfUpdatablePasses);
+    delete(
+      '/v1/devices/<deviceID>/registrations/<passTypeID>/<serial>',
+      stopNotifications,
+    );
+    post(
+      '/v1/devices/<deviceID>/registrations/<passTypeID>/<serial>',
+      setupNotifications,
+    );
     post('/v1/log', logMessages);
   }
 }
@@ -27,10 +37,9 @@ FutureOr<Response> getLatestVersion(
   String identifier,
   String serial,
 ) async {
-  var header = request.headers['Authorization'];
-  var token = header?.split(' ').lastOrNull;
+  var token = request.getApplePassToken();
 
-  if (header?.startsWith('ApplePass ') == false || token == null) {
+  if (token == null) {
     return Response.unauthorized(null);
   }
 
@@ -47,22 +56,44 @@ FutureOr<Response> getLatestVersion(
 // POST request
 /// URL must end with "v1/log"
 FutureOr<Response> logMessages(Request request) async {
-  if (request.method == 'POST') {
-    final content = await request.readAsString();
-    DevPassKitBackend().logMessage(jsonDecode(content) as Map<String, dynamic>);
-  }
-  return Response.notFound(null);
+  final content = await request.readAsString();
+  DevPassKitBackend().logMessage(jsonDecode(content) as Map<String, dynamic>);
+  return Response.ok(null);
 }
 
 // POST request
 /// URL must end with "v1/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}"
-FutureOr<Response> setupNotifications(Request request) {
+FutureOr<Response> setupNotifications(
+  Request request,
+  String deviceId,
+  String passTypeId,
+  String serialNumber,
+) {
   return Response.notFound(null);
 }
 
-// DELETE request
-/// URL must end with "v1/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}"
-FutureOr<Response> stopNotifications(Request request) {
+/// Unregister
+///
+/// unregister a device to receive push notifications for a pass
+///
+/// DELETE /v1/devices/<deviceID>/registrations/<passTypeID>/<serialNumber>
+/// Header: Authorization: ApplePass <authenticationToken>
+///
+/// server action: if the authentication token is correct, disassociate the device from this pass
+/// server response:
+/// --> if disassociation succeeded: 200
+/// --> if not authorized: 401
+FutureOr<Response> stopNotifications(
+  Request request,
+  String deviceId,
+  String passTypeId,
+  String serialNumber,
+) {
+  var token = request.getApplePassToken();
+
+  if (token == null) {
+    return Response.unauthorized(null);
+  }
   return Response.notFound(null);
 }
 
@@ -81,12 +112,22 @@ FutureOr<Response> stopNotifications(Request request) {
 /// --> if there are matching passes: 200, with JSON payload: { "lastUpdated" : <new tag>, "serialNumbers" : [ <array of serial numbers> ] }
 /// --> if there are no matching passes: 204
 /// --> if unknown device identifier: 404
-FutureOr<Response> getListOfUpdatablePasses(Request request) {
+FutureOr<Response> getListOfUpdatablePasses(
+  Request request,
+  String deviceId,
+  String typeId,
+) {
   return Response.notFound(null);
 }
 
-// POST request
-/// URL must end with "v1/passes/{passTypeIdentifier}/{serialNumber}/personalize"
-FutureOr<Response> personalize(Request request) {
-  return Response.notFound(null);
+extension on Request {
+  String? getApplePassToken() {
+    var header = headers['Authorization'];
+    if (header?.startsWith('ApplePass ') == true) {
+      var token = header?.split(' ').lastOrNull;
+      return token;
+    }
+
+    return null;
+  }
 }
