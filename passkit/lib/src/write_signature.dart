8b8028399ba1638e3b2f2b7eac0b514cb79b2ea9
Jonas Uekötter <ueman@users.noreply.github.com>
2024-09-15 09:10:06 +0200

    formatting

--- passkit/lib/src/write_signature.dart
+++ passkit/lib/src/write_signature.dart
@@ -6,32 +6,26 @@ import 'package:pkcs7/pkcs7.dart';
 import 'package:pointycastle/pointycastle.dart';
 
 Uint8List writeSignature(
-  String userCertificate,
-  Uint8List manifestBytes,
+  String userCertificate, // this is private key and certificate in one file
+  Uint8List
+      manifestBytes, // has of the manifest file or maybe content, not sure
 ) {
   final privateKey = _createPrivateKey(userCertificate);
   final issuer = X509.fromPem(userCertificate);
 
   final pkcs7Builder = Pkcs7Builder();
-  // Add one or more certificates
+
+  pkcs7Builder.addCertificate(issuer);
   pkcs7Builder.addCertificate(wwdrG4);
 
-  // Create a signature information object
-  final signerInfo =
-      Pkcs7SignerInfoBuilder.rsa(issuer: issuer, privateKey: privateKey);
-
-  // Add the digest to sign
-  signerInfo.addSMimeDigest(digest: manifestBytes, signingTime: DateTime.now());
-
-  // Add the signature information
-  pkcs7Builder.addSignerInfo(signerInfo);
-
-  // Add a certificate revocation list
-  pkcs7Builder.addCRL(
-    CertificateRevocationList.fromDer(
-      Uint8List.fromList(worldwide_Developer_Relations_G4),
-    ),
+  final signerInfo = Pkcs7SignerInfoBuilder.rsa(
+    issuer: issuer,
+    privateKey: privateKey,
+    digestAlgorithm: HashAlgorithm.sha1,
   );
+
+  signerInfo.addSMimeDigest(digest: manifestBytes, signingTime: DateTime.now());
+  pkcs7Builder.addSignerInfo(signerInfo);
 
   final pkcs7 = pkcs7Builder.build();
   return pkcs7.der;
