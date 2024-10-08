24246097812a379450ac90ce518bf344692a3fd0
Jonas Uekötter <ueman@users.noreply.github.com>
2024-10-08 17:35:01 +0200

    enable lints again

--- passkit/lib/src/pkpass/pass_data.dart
+++ passkit/lib/src/pkpass/pass_data.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:csslib/parser.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:passkit/src/crypto/certificate_extension.dart';
--- passkit/lib/src/pkpass/semantics.dart
+++ passkit/lib/src/pkpass/semantics.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 import 'package:passkit/src/pkpass/semantic_tag_type.dart';
 
