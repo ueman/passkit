c9aab94d53710022c1a0a249e4e8ac2a74c880e9
Jonas Uekötter <ueman@users.noreply.github.com>
2024-10-13 17:06:34 +0200

    don't export order UI from passkit_ui just yet

--- app/lib/import_order/import_order_page.dart
+++ app/lib/import_order/import_order_page.dart
@@ -5,7 +5,8 @@ import 'package:content_resolver/content_resolver.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:passkit/passkit.dart';
-import 'package:passkit_ui/passkit_ui.dart';
+// ignore: implementation_imports
+import 'package:passkit_ui/src/order/order_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PkOrderImportSource {
